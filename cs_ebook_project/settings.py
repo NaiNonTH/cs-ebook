943777cf16bcec616c6c40c68e7822b51639fe70
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2026-03-29 17:15:00 +0700

    add beautiful ui

--- cs_ebook_project/settings.py
+++ cs_ebook_project/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/ebooks/',
+    BASE_DIR / 'static/market/'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
